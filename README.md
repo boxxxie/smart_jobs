7632a3d9631a097986ec2f54c5f5a3365ac7d69d
Paul Iannazzo <somethingitalian@gmail.com>
2012-10-25 16:25:28 -0400

    first post!

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-smart_jobs
+Smart Jobs
 ==========
 
-interface over node messagehub, which is an interface over RabbitMQ for doing simple job processing.
+This is a small wrapper over messagehub.js that adds queue callbacks for jobs.
+
+example use:
+````
+var image_processing_hub = hub(args.rabbitmq,'simple.image.processing');
+var image_processing_job = smart_job(image_processing_hub,'process pdf');
+var image_processing_done = smart_job(image_processing_hub,'done with doc');
+````
+
+in the above example we create a hub via message hub, this sets the rabbitMQ config and gives our messages a namespace of 'simple.image.processing'
+
+when we create a smart job ```image_processing_job``` it generates further name spaces 'process pdf' and 'finished process pdf'
+
+Posting Jobs
+------------
+````
+image_processing_done.post(job);
+````
+the above is an example of what one would do when they want to post a job. in this case the job is being posted to 'done with doc' queue in the 'simple.image.processing. name-space.
+
+Working on Jobs
+---------------
+````
+image_processing_job
+  .worker(function(job,done){...})
+````
+setting up a worker is just like message hub, minus the name-space. the done function takes a response message that is sent to the issuer of the job.
+
+Responding to finished Jobs
+---------------------------
+````
+image_processing_job
+     .response(function(job_response,done){...});
+````
+The response is used when listening to a finished job. The job_response argument is sent via the worker that finished the job. otherwise ```.response``` is the same as ```.worker``` except that it's ```done``` callback doesn't take arguments.
---
+++ smart_jobs.js
@@ -0,0 +1,48 @@
+var _ = require('underscore');
+require('underscore_extended');
+//var hub = require('messagehub');
+
+function smart_job(hub,name){
+  const job_name = name;
+  const finished_job_name = 'finished '+ job_name;
+  console.log('setting up job poster for:', job_name);
+  return {
+    name:job_name,
+    post:function(job){
+      const posted_job = job || {};
+      console.log('posting job:',job_name, posted_job);
+      hub.job(job_name,posted_job);
+    },
+    response:function(job_finished){       //a response to a finished job
+      console.log('setting up response listener for:', finished_job_name);
+      hub.worker(finished_job_name, function(response,done){
+        console.log('acknowledging finished job', job_name);
+        job_finished(response,done);
+      });
+    },
+    no_response:function(){
+      this.response(function(resp,done){done()});
+    },
+    worker:function(job_finished){ //after all work is finished, a proper
+      //response should be issued that will be caught by the
+      //'response' function
+      console.log('setting up worker for:', job_name);
+      var worker = this;
+      hub.worker(job_name, function(job,done){
+
+        function done_job(response){
+          done();//tell rabbit that we are done with the current queue
+          //item
+          const job_message = response ? _.combine(job,response) : job;
+          console.log('issuing response for job', job_message);
+          hub.job(finished_job_name,job_message);
+        }         
+
+        console.log('finished job', job_name);
+        job_finished(job,done_job);
+      });
+    }
+  } 
+}
+
+module.exports = smart_job;
